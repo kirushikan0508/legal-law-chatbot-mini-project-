d8491f7e1378ba6da275f6e0a529c5d01c31ec21
kirushikan <kirushikankiru01@gmail.com>
2025-10-02 09:22:18 +0530

    load environmental

---
+++ services/app.py
@@ -0,0 +1,23 @@
+from flask import Flask , render_template, jsonify, request
+from src.helper import download_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import System_prompt
+import os
+
+app = Flask(__name__)
+
+
+
+# -------------------- Load environment variables --------------------
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["GEMINI_API_KEY"] = GEMINI_API_KEY
