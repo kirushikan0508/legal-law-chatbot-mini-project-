c42dc8a65d2c956ff4a119f21830d548cfe62b9c
shefa-ul123 <shefaul810@gmail.com>
2025-11-18 07:43:00 +0530

    modification

--- frontend/src/Pages/Document/Document.jsx
+++ frontend/src/Pages/Document/Document.jsx
@@ -1,10 +1,13 @@
 import DocumentGenerator from "../../Components/Document Generator/SelectTemplate/DocumentGenerator";
+import Navbar from "../../Components/Navbar/Navbar";
 import "./document.css";
 
 function Document(){
 
     return(
         <div className="document-container">
+
+            <Navbar />
             <DocumentGenerator/>
         </div>
     );
